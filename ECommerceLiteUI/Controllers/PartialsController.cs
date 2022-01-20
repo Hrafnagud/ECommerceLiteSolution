7afa9d4ff9f063cff7ead526ab6ad0363a874df5
Emre Ç <superbevent@hotmail.com>
2022-01-20 15:00:47 +0300

    Partial Views for admin panel has been initiated

---
+++ ECommerceLiteUI/Controllers/PartialsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class PartialsController : BaseController
+    {
+        public PartialViewResult AdminSideBarResult()
+        {
+            //TODO: fullname will be received
+            TempData["Fullname"] = "";
+            return PartialView("_PartialAdminSideBar");
+        }
+        public PartialViewResult AdminSideBarMenuResult()
+        {
+            return PartialView("_PartialAdminSideBarMenu");
+        }
+    }
+}
