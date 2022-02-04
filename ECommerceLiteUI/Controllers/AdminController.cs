4f2789443ca00b0067c9c72d9cd0f0434adb6753
Emre Ç <superbevent@hotmail.com>
2022-02-04 14:44:33 +0300

    Doughnut pie chart added to Dashboard2.

--- ECommerceLiteUI/Controllers/AdminController.cs
+++ ECommerceLiteUI/Controllers/AdminController.cs
@@ -7,10 +7,12 @@ using ECommerceBusinessLogicLayer.Repository;
 
 namespace ECommerceLiteUI.Controllers
 {
+
     [Authorize(Roles = "Admin")]
     public class AdminController : BaseController
     {
         OrderRepo orderRepo = new OrderRepo();
+        CategoryRepo categoryRepo = new CategoryRepo();
 
         // GET: Admin
         public ActionResult Dashboard()
@@ -18,7 +20,20 @@ namespace ECommerceLiteUI.Controllers
             var orderList = orderRepo.GetAll();
             var newOrderCount = orderList.Where(x => x.RegisterDate >= DateTime.Now.AddMonths(-1)).ToList().Count();
             ViewBag.NewOrderCount = newOrderCount;
+            var model = categoryRepo.GetBaseCategoriesProductCount();
             return View();
+        }
+
+        public ActionResult Dashboard2()
+        {
+            var orderList =
+               orderRepo.GetAll();
+            //1 aylık sipariş sayısı
+            var newOrderCount = orderList.Where(x => x.RegisterDate >= DateTime.Now.AddMonths(-1)).ToList().Count;
+            ViewBag.NewOrderCount = newOrderCount;
+
+            var model = categoryRepo.GetBaseCategoriesProductCount();
+            return View(model);
         }
 
         [AllowAnonymous]
