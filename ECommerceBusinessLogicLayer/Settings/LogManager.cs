c698efc1ca6f850c25b39fd2f86f34bb2a21cfbc
Emre Ç <superbevent@hotmail.com>
2022-02-04 09:00:21 +0300

    Ex log commenced.

---
+++ ECommerceBusinessLogicLayer/Settings/LogManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBusinessLogicLayer.Settings
+{
+    public class LogManager
+    {
+        public static void LogMessage(string message, string userInfo="", string pageInfo = "")
+        {
+            string fileName = "ECommerceLite" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+            {
+                StreamWriter writer = new StreamWriter(stream);
+            }
+        }
+    }
+}
